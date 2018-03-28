2e59c3d2123100b8507d817749b0037a38fdd265
Emrys Hong <35634481+Emrys-Hong@users.noreply.github.com>
2018-03-28 21:05:48 +0800

    Create dynamic_programming.py

---
+++ python/self_learning/dynamic_programming.py
@@ -0,0 +1,65 @@
+# dynamic programming
+
+# using recursion function would usually take O(2^n) time but if we use the memo function it will take O(n) time just like the bottom up solution
+# A memoized solution
+def fib_2(n, memo):
+    if memo[n] is not None:
+        return memo[n]
+    if n == 1 or n == 2:
+        result = 1
+    else:
+        result = fib_2(n-1, memo) + fib_2(n-2, memo)
+    memo[n] = result
+    return result
+
+def fib_memo(n):
+    memo = [None] * (n + 1)
+    return fib_2(n, memo)
+____________________________________________________________________________________________________________________________________
+# Create a memoisation decorator
+def memoise(f):
+    memo = {}
+    def checker(x):
+        if x not in memo:            
+            memo[x] = f(x)
+        return memo[x]
+    return checker
+# And apply it! Done!
+@memoise
+def fib(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return fib(num - 1) + fib(num - 2)
+________________________________________________________________________________________________________________________________
+class Memoize:
+    def __init__(self, fn):
+        self.fn = fn
+        self.memo = {}
+    def __call__(self, *args):
+        if args not in self.memo:
+        self.memo[args] = self.fn(*args)
+        return self.memo[args]
+
+@Memoize
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+_______________________________________________________________________________________________________________________________
+# A bottom-up solution
+def fib_bottom_up(n):
+    if n == 1 or n == 2:
+        return 1
+    bottom_up = [None] * (n+1)
+    bottom_up[1] = 1
+    bottom_up[2] = 1
+    for i in range(3, n+1):
+        bottom_up[i] = bottom_up[i-1] + bottom_up[i-2]
+    return bottom_up[n]

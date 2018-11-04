d66b4671a69072fc96985c37a50b71a0e42ed48b
Emrys-Hong <hongpengfei.emrys@gmail.com>
2018-11-04 16:46:27 +0800

    updated utilities

--- python/utils/download_images_csv_url.py
+++
@@ -1,9 +0,0 @@
-
-import urllib.request
-import pandas as pd 
-
-df = pd.read_excel("parcel_data.xlsx")
-urls = df.ix[:,'Parcel Label Photo']
-
-for i,url in enumerate(urls):
-    urllib.request.urlretrieve(url,'images/'+str(i)+'.png')
